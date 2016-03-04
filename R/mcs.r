2a30a9b7c8f67a7cd37c30fd026a43b9cef87f92
dkneis <david.kneis@tu-dresden.de>
2016-03-04 14:11:04 +0000

    REV: method 'mcs' now returns matrices rather than data frames

--- R/mcs.r
+++ R/mcs.r
@@ -13,8 +13,8 @@
 #' @param silent If \code{TRUE}, diagnostic messages are suppressed.
 #' @param ... Additional arguments passed to function \code{fn}.
 #'
-#' @return A list of 3 elements. Elements \code{p} and \code{out} are both data
-#'   frames with \code{nRuns} + 1 rows. \code{p} holds the tested parameter values
+#' @return A list of 3 elements. Elements \code{p} and \code{out} are matrices
+#'   with \code{nRuns} + 1 rows. \code{p} holds the tested parameter values
 #'   with column names taken from the 'name' field of \code{ranges}.
 #'   \code{out} holds the return values of \code{fn}. Each row in \code{out}
 #'   corresponds to the same row of \code{p}. In the common case where \code{fn}
@@ -24,7 +24,7 @@
 #'   holding the times spent on the evaluation of \code{fn}.
 #'
 #' @note If \code{fn} generated an error (or a warning) when called with a
-#'   parameter set, the corresponding row in the result data frame \code{out}.
+#'   parameter set, the corresponding row in the result matrix \code{out}.
 #'   is set to \code{NA}. The same is true for the corresponding element of
 #'   the returned vector \code{cpu}.
 #'
@@ -45,8 +45,8 @@
 #' )
 #' x= mcs(fn=objfun, p=p, obs=obs)
 #' layout(matrix(1:2, ncol=2))
-#' plot(x$p[,"slope"], x$out$sse, xlab="slope", ylab="SSE")
-#' plot(x$p[,"intercept"], x$out$sse, xlab="intercept", ylab="SSE")
+#' plot(x$p[,"slope"], x$out[,"sse"], xlab="slope", ylab="SSE")
+#' plot(x$p[,"intercept"], x$out[,"sse"], xlab="intercept", ylab="SSE")
 #' layout(matrix(1))
 
 mcs= function(fn, p, nRuns=10, silent=TRUE, ...) {
@@ -66,9 +66,8 @@ mcs= function(fn, p, nRuns=10, silent=TRUE, ...) {
   # Sample parameters
   if (!silent)
     print("creating sample")
-  tmp= improvedLHS(n=nRuns, k=nrow(p))
-  colnames(tmp)= p$name
-  prand= as.data.frame(tmp)
+  prand= improvedLHS(n=nRuns, k=nrow(p))
+  colnames(prand)= p$name
   for (i in 1:nrow(p)) {
     prand[,i]= p[i,"min"] + prand[,i] * (p[i,"max"] - p[i,"min"])
   }
@@ -80,10 +79,11 @@ mcs= function(fn, p, nRuns=10, silent=TRUE, ...) {
     print(paste0("initial run with defaults"))
   t0= Sys.time()
   tmp= fn(setNames(p$default,p$name),...)
-  if (is.null(names(tmp)) || (any(names(tmp) == "")))
-    stop("'fn' does not return a named vector")
-  out= data.frame(matrix(tmp, nrow=1))
-  names(out)= names(tmp)
+  if (!is.numeric(tmp) || is.null(names(tmp)) || (any(names(tmp) == "")))
+    stop("'fn' does not return a named numeric vector")
+  out= matrix(NA, ncol=length(tmp), nrow=nRuns+1)
+  colnames(out)= names(tmp)
+  out[1,]= tmp
   t1= Sys.time()
   tTotal= nRuns * as.numeric(difftime(t1, t0, units="secs"))
   tElapsed= 0
@@ -97,15 +97,15 @@ mcs= function(fn, p, nRuns=10, silent=TRUE, ...) {
     t0= Sys.time()
     ok= FALSE
     tryCatch({
-      tmp= fn(setNames(unlist(prand[i,]),names(prand)),...)
-      out= rbind(out,tmp)
+      tmp= fn(setNames(prand[i,],colnames(prand)),...)
+      out[i+1,]= tmp
       ok= TRUE
     }, error= function(e) {
       print(e)
-      out= rbind(out,rep(NA, ncol(out)))
+      out[i+1,]= rep(NA, ncol(out))
     }, warning= function(w) {
       print(w)
-      out= rbind(out,rep(NA, ncol(out)))
+      out[i+1,]= rep(NA, ncol(out))
     })
     t1= Sys.time()
     if (ok)
@@ -115,7 +115,7 @@ mcs= function(fn, p, nRuns=10, silent=TRUE, ...) {
   }
 
   # Add default parameters to table of parameters
-  prand= rbind(as.data.frame(as.list(setNames(p$default, p$name))), prand)
+  prand= rbind(p$default, prand)
 
   # Return tested parameter values and function results
   return(list(p=prand, out=out, cpu=cpu))
